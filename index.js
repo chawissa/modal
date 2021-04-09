f943c6196e6f43285c0ee835f041e5cf1207a3ae
Charissa Ramirez <chawissa@gmail.com>
2021-04-08 21:54:20 -0400

    Show/hide modal with buttons

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
